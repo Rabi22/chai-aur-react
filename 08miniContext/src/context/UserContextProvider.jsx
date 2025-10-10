020cb7c698921771ac1e98d9307fda1534c32643
Rabi22 <75093181+Rabi22@users.noreply.github.com>
2025-10-11 00:57:56 +0530

    Update UserContextProvider.jsx

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -11,3 +11,6 @@ const UserContextProvider = ({children}) => {
 }
 
 export default UserContextProvider
+
+// It creates a piece of internal memory (state) to track the current user (user) and a function (setUser) that any component can use to change that user data.
+// It then returns the <UserContext.Provider> component, making the current user data (user) and the update function (setUser) available to all components that are wrapped inside of it ({children}).
